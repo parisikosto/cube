19093514f7602dbef1d879d0c26074d313414740
Paris Kostopoulos <parisikosto@gmail.com>
2026-04-16 21:56:52 +0300

    feat(install.sh): add bash completion in install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-# Define the latest release URL
 LATEST_RELEASE="https://github.com/parisikosto/cube/releases/latest/download/cube-linux-amd64"
 
-# Download the binary
-echo "Downloading cube-linux-amd64..."
-curl -L -o cube $LATEST_RELEASE
+echo "Downloading cube..."
+curl -fsSL -o cube $LATEST_RELEASE
 
-# Move it to /usr/local/bin
 chmod +x cube
 sudo mv cube /usr/local/bin/cube
+
+echo "Setting up shell completion..."
+if [ -d /etc/bash_completion.d ]; then
+  cube completion bash | sudo tee /etc/bash_completion.d/cube > /dev/null
+fi
 
 echo "Installation complete! Run 'cube --help' to get started."
